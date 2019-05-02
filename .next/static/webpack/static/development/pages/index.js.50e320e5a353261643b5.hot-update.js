a26977b05a2531f00569216f6382c59da72f02c2
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-01 21:10:29 -0600

    setup a layout for our components and brought in bootswatch

---
+++ .next/static/webpack/static/development/pages/index.js.50e320e5a353261643b5.hot-update.js
@@ -0,0 +1,40 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Layout.js":
+/*!******************************!*\
+  !*** ./components/Layout.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _Navbar__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Navbar */ "./components/Navbar.js");
+var _jsxFileName = "/Users/tayte/Desktop/dev/youtube/nextjs/components/Layout.js";
+
+
+
+var Layout = function Layout(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Navbar__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }), props.children);
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Layout);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.50e320e5a353261643b5.hot-update.js.map
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+//Components
+import Layout from '../components/Layout';
+
+const About = () => (
+    <Layout>
+        <div>
+            <h1>About Bitzprice</h1>
+            <p>Our application to view bitcoin prices</p>
+        </div>
+    </Layout>
+);
+
+export default About;
